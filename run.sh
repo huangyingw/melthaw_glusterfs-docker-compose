a5d17f9809eed6d4ca60ce32b25f7ad261652df2
Ying Huang <huangyingw@gmail.com>
2021-04-05 22:59:36 -0700

    n

--- run.sh
+++ run.sh
@@ -12,3 +12,5 @@ docker-compose exec glusterfs1 gluster volume start test-volume
 docker-compose exec glusterfs1 setfacl -m u:1000:rwx /data/glusterfs/test
 docker-compose exec glusterfs2 setfacl -m u:1000:rwx /data/glusterfs/test
 docker-compose exec glusterfs1 tail -f /var/log/glusterfs/bricks/data-glusterfs-test.log
+docker-compose exec glusterfs1 gluster peer status
+docker-compose exec glusterfs1 gluster pool list
